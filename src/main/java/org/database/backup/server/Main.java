3803391faf285c5b10e5dd60dc3a75bd812f7f92
nolo <lemashijhb024@student.wethinkcode.co.za>
2025-08-27 13:00:47 +0200

    requiremments

--- src/main/java/org/database/backup/server/Main.java
+++ src/main/java/org/database/backup/server/Main.java
@@ -1,4 +1,4 @@
-package org.database.backup;
+package org.database.backup.server;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
