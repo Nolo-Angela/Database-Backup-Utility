0c2a741c3dd640a63efc8756b766ef3ff99e2bca
nolo <lemashijhb024@student.wethinkcode.co.za>
2025-08-28 12:33:04 +0200

    client socket

--- src/main/java/org/database/backup/client/client.java
+++ src/main/java/org/database/backup/client/client.java
@@ -1,4 +1,45 @@
 package org.database.backup.client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class client {
+    private static String host = "localhost";
+    private static int port = 5000;
+
+    public static void main(String[] args) {
+
+        try(Socket socket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ){
+            out.println("Hello from client");
+
+            String line = in.readLine();
+            System.out.println(line);
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void listenThread(BufferedReader out) {
+
+        Thread listen = new Thread(() -> {
+            while(true){
+                try{
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+    }
+
+    private static void sendThread(String message, PrintWriter out) {}
 }
