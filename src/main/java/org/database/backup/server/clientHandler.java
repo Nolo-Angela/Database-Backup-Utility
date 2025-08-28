f93423c650f59e30d3b867284f5d14482d021b53
nolo <lemashijhb024@student.wethinkcode.co.za>
2025-08-28 12:34:38 +0200

    clientHandler

--- src/main/java/org/database/backup/server/clientHandler.java
+++ src/main/java/org/database/backup/server/clientHandler.java
@@ -1,4 +1,32 @@
 package org.database.backup.server;
 
-public class clientHandler {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class clientHandler implements Runnable {
+
+    private Socket clientSocket;
+
+    public clientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+
+    }
+
+    public void run() {
+        try(
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ){
+
+            String read = in.readLine();
+            System.out.println(read);
+
+            out.println("Hello from server");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
