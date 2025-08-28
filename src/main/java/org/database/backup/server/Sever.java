92b25b2e0de379422b2775e7a852114a34a3ce01
nolo <lemashijhb024@student.wethinkcode.co.za>
2025-08-28 12:34:12 +0200

    start server method

--- src/main/java/org/database/backup/server/Sever.java
+++ src/main/java/org/database/backup/server/Sever.java
@@ -1,17 +1,40 @@
 package org.database.backup.server;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Sever {
+    private static ServerSocket serverSocket;
+    private static int port= 5000;
+
+
+    public static void main(String[] args) throws IOException {
+        serverSocket = new ServerSocket(port);
+        startServer();
+
+
     }
+
+    private static void startServer(){
+        System.out.println("Server started");
+
+        Thread serverThread = new Thread(()-> {
+           // while (true) {
+                try{
+                    Socket socket = serverSocket.accept();
+                    System.out.println("Client connected: " + socket.getInetAddress());
+
+                    new Thread(new clientHandler(socket)).start();
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+           // }
+        });
+        serverThread.start();
+    }
+
+
 }
